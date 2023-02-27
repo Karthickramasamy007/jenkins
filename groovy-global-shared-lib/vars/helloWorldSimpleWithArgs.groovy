3e68248e04c984f7d5c39710655adbdbbf09f8ae
Karthickramasamy007 <10cse61@gmail.com>
2023-02-27 19:19:12 +0000

    inint

--- groovy-global-shared-lib/vars/helloWorldSimpleWithArgs.groovy
+++ groovy-global-shared-lib/vars/helloWorldSimpleWithArgs.groovy
@@ -1,8 +1,6 @@
 def call(String name, String dayOfWeek) {
   sh "echo Hello World ${name}. It is ${dayOfWeek}."
   sh "Below should run python code"
-  sh """
-    python3 --version
-  """
+  sh(script: "pwd")
 
 }
