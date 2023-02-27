7547e41befb25b62cb1d5ca179a4fbc0e9a6d122
Karthickramasamy007 <10cse61@gmail.com>
2023-02-27 22:35:50 +0000

    inint

--- groovy-global-shared-lib/vars/helloWorldSimpleWithArgs.groovy
+++ groovy-global-shared-lib/vars/helloWorldSimpleWithArgs.groovy
@@ -1,3 +1,10 @@
 def call(String name, String dayOfWeek) {
   sh "echo Hello World ${name}. It is ${dayOfWeek}."
+  script {
+                   GIT_COMMIT_EMAIL = sh (
+                        script: 'pwd',
+                        returnStdout: true
+                    ).trim()
+                    echo "Git committer email from groogy karthi: ${GIT_COMMIT_EMAIL}"
+                }
 }
