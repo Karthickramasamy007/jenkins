f352ff12188b67a67b3d28e164cd1e0a0e8ff31c
Karthickramasamy007 <10cse61@gmail.com>
2023-02-27 18:40:21 +0000

    inint

--- groovy-global-shared-lib/vars/helloWorldSimpleWithArgs.groovy
+++ groovy-global-shared-lib/vars/helloWorldSimpleWithArgs.groovy
@@ -2,6 +2,9 @@ def call(String name, String dayOfWeek) {
   sh "echo Hello World ${name}. It is ${dayOfWeek}."
   sh "Below should list all PWD and files"
   sh "pwd"
-  sh "Below should list all files"
-  sh "ls -l"
+  sh "Below should renam file hai.txt to hello.txt"
+  sh "mv hai.txt hello.txt"
+  sh "Below should renam file hgggggggg.txt to hello.txt"
+  sh "mv hgggggggg.txt hello.txt"
+
 }
