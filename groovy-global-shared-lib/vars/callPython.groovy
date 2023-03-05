106a176b6e3cceb994a07b0bfd3126569b4248af
Karthickramasamy007 <10cse61@gmail.com>
2023-03-05 13:34:52 +0000

    added python

--- groovy-global-shared-lib/vars/callPython.groovy
+++ groovy-global-shared-lib/vars/callPython.groovy
@@ -1,10 +1,3 @@
 def callPython() {
   sh "echo Jenkins callPython reached..."
-  script {
-                   GIT_COMMIT_EMAIL = sh (
-                        script: 'python helloPython.py',
-                        returnStdout: true
-                    ).trim()
-                    echo "Git committer email from groogy karthi: ${GIT_COMMIT_EMAIL}"
-                }
 }
